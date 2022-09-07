dc7a256578590e1c34c5b10a55eadc36563811a1
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-09-07 14:07:56 +0100

    reinstalled node_modules, test passes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,9 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../components/App';
 import forecast from '../data/forecast.json';
 
 describe("App", () => {
-  test('renders App component', () => {
+  it('renders App component', () => {
     render(<App location={forecast.location} />);
     const h1Element = screen.getByText(/Manchester, UK/i);
     expect(h1Element).toBeInTheDocument();
