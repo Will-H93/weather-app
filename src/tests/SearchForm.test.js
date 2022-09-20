a97eacfbf4e481219ae32da8e0232205ea9a3d14
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-09-20 21:37:11 +0100

    SearchForm test passes

--- src/tests/SearchForm.test.js
+++ src/tests/SearchForm.test.js
@@ -3,7 +3,12 @@ import { render } from "@testing-library/react";
 import SearchForm from "../components/SearchForm";
 
 it("renders", () => {
-    const { asFragment } = render(<SearchForm />);
+  const validProps = {
+    searchText: "Test Search Text",
+    setSearchText: jest.fn(),
+    onSubmit: jest.fn(),
+  }
+    const { asFragment } = render(<SearchForm searchText={ validProps.searchText } setSearchText={ validProps.setSearchText } onSubmit={ validProps.onSubmit }/>);
 
     expect(asFragment()).toMatchSnapshot();
   });
